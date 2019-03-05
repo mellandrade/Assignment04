576c36cda46839f418d80ee0f0fc883f6b4c20dd
mell andrade <mellisaslandrade@gmail.com>
2019-03-05 18:40:59 +0000

    Update 02

--- js/01-arrays-practice.js
+++ js/01-arrays-practice.js
@@ -1,23 +1,80 @@
 /*eslint-env broswer*/
 /*jslint devel: true */
+
 //STEP 1
-var movies = ["French Kiss", "Serendipity", "Love Actually", "Outbreak", "Pride and Prejudice"];
-window.console.log(movies[1]);
+/* var movies = ["French Kiss", "Serendipity", "Love Actually", "Outbreak", "Pride and Prejudice"];
+window.console.log(movies[1]); */
 
 //STEP 2
+/* var movies = new Array(5);
+movies[0] = "French Kiss";
+movies[1] = "Serendipity";
+movies[2] = "Love Actually";
+movies[3] = "Outbreak";
+movies[4] = "Pride and Prejudice";
+window.console.log(movies[0]); */
 
 //STEP 3
+/* var movies = new Array(5);
+movies[0] = "French Kiss";
+movies[1] = "Serendipity";
+movies[2] = "Love Actually";
+movies[3] = "Outbreak";
+movies[4] = "Pride and Prejudice";
+movies[5] = "Emma";
+window.console.log(movies.length); */
 
 //STEP 4
+/* var movies = ["French Kiss", "Serendipity", "Love Actually", "Outbreak", "Pride and Prejudice"];
+delete movies[0];
+window.console.log(movies); */
 
 //STEP 5
+/* var i;
+var movies = ["French Kiss", "Serendipity", "Love Actually", "Outbreak", "Pride and Prejudice", "Thumbelina", "Overboard"];
+for (i = 0; i < movies.length; i += 1) {
+    window.console.log(movies[i]);
+} */
 
 //STEP 6
+/* Array.prototype.myname = function () {
+    "use strict";
+    
+};
+var i;
+var movies = ["French Kiss", "Serendipity", "Love Actually", "Outbreak", "Pride and Prejudice", "Thumbelina", "Overboard"];
+for (i in movies) {
+    if (movies.hasOwnProperty(i)) {
+        window.console.log(movies[i]);
+    }
+} */
 
 //STEP 7
+/* var i;
+var movies = ["French Kiss", "Serendipity", "Love Actually", "Outbreak", "Pride and Prejudice", "Thumbelina", "Overboard"];
+for (i = 0; i < movies.length; i += 1) {
+    window.console.log(movies.sort());
+} */
+// NEED TO FIGURE OUT
+/* 
+var i;
+var movies = ["French Kiss", "Serendipity", "Love Actually", "Outbreak", "Pride and Prejudice", "Thumbelina", "Overboard"];
+for (i in movies) {
+    window.console.log(movies.sort());
+} */
 
 //STEP 8
-
+/*
+// NEED TO FIGURE OUT
+var i;
+var movies = ["French Kiss", "Serendipity", "Love Actually", "Outbreak", "Pride and Prejudice", "Thumbelina", "Overboard"];
+var leastFavMovies =["Sin City", "Ted", "White Chicks" ]
+for (i = 0; i < movies.length; i += 1) {
+    for (i = 0; i < leastFavMovies.length; i += 1) {
+        window.console.log(movies[i] + "<br>" + leastFavMovies);  
+}
+}
+*/
 //STEP 9
 
 //STEP 10
